9619d0d2a93c3981452b316c4ce833e621984a8e
Niklas Korz <niklas@niklaskorz.de>
2026-05-19 14:13:47 +0200

    Update yarn

--- plugin/fix-dist.mjs
+++
@@ -1,6 +0,0 @@
-import fs from "node:fs";
-
-const source = fs.readFileSync("dist/plugin-yarnpnp2nix.js", {
-    encoding: "utf-8",
-});
-fs.writeFileSync("dist/plugin-yarnpnp2nix.js", source.replaceAll('w("node:', 'w("'), { encoding: "utf-8" });
